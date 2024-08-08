ec604e83fe937b0d9ae88fa7705913eb2a9772c0
sabrina <sabrina@verymountain.fr>
2024-08-08 11:08:35 +0200

    :hammer: Display tags by filters

--- scripts/appliancesFilter.js
+++ scripts/appliancesFilter.js
@@ -1,22 +1,31 @@
-export function appliancesFilter(recipes, onTagSelect) {
+// export function appliancesFilter(recipes, onTagSelect) {
 
-    // affiche / ferme le dropdown 
-    document.getElementById('menuButtonApplience').addEventListener('click', () => {
-        const menuItems = document.getElementById('menuItemsApplience');
-        const menuButton = document.getElementById('menuButtonApplience');
+//     // affiche / ferme le dropdown
+//     document.getElementById('menuButtonApplience').addEventListener('click', () => {
+//         const menuItems = document.getElementById('menuItemsApplience');
+//         const menuButton = document.getElementById('menuButtonApplience');
 
+//         menuItems.classList.toggle('hidden');
+//         const isOpen = !menuItems.classList.contains('hidden');
+
+//         if (isOpen) {
+//             menuButton.classList.add('menu-open');
+//         } else {
+//             menuButton.classList.remove('menu-open');
+//         }
+//     });
+
+// }
+
+
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    const button = dropdown.querySelector('button');
+    const menuItems = dropdown.querySelector('.menu-items');
+
+    button.addEventListener('click', () => {
         menuItems.classList.toggle('hidden');
         const isOpen = !menuItems.classList.contains('hidden');
-
-        if (isOpen) {
-            menuButton.classList.add('menu-open');
-            console.log('oooooooooo')
-        } else {
-            menuButton.classList.remove('menu-open');
-            console.log('aaaaaaaaaa')
-
-        }
+        button.classList.toggle('menu-open', isOpen);
     });
 
-
-}
+});
