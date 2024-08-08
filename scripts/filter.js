0520d330fe2867c3cd45d6fb1a2821190cd60428
sabrina <sabrina@verymountain.fr>
2024-08-08 14:45:32 +0200

    :hammer: Add all dropdowns and filters

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,15 +1,68 @@
+// export function filterRecipes(recipes, query, tags = []) {
+//     //utilise filter pour parcourir et filtrer toutes les recettes
+//     return recipes.filter(recipe => {
+//         // vérifie si le nom, la description, les ingrédients correspondent à la requête
+//         const foundBySearch =
+//             recipe.name.toLowerCase().includes(query) ||
+//             recipe.description.toLowerCase().includes(query) ||
+//             recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(query));
+
+//         const foundByTags = tags.length === 0 || tags.every(tag =>
+//             recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase() === tag.toLowerCase())
+//         );
+
+//         return foundBySearch && foundByTags;
+//     });
+// }
+
+
+// export function filterRecipes(recipes, query, tags = []) {
+//     return recipes.filter(recipe => {
+//         // Vérifie si le nom, la description, ou les ingrédients correspondent à la requête
+//         const foundBySearch =
+//             recipe.name.toLowerCase().includes(query) ||
+//             recipe.description.toLowerCase().includes(query) ||
+//             recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(query));
+
+
+//         // Vérifie si la recette contient tous les tags d'ingrédients
+//         const foundByIngredientTags = tags.length === 0 || tags.every(tag =>
+//             recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase() === tag.toLowerCase())
+//         );
+
+//         // Vérifie si la recette contient tous les tags d'appareils
+//         const foundByApplianceTags = tags.length === 0 || tags.every(tag =>
+//             recipe.appliance(recipe => recipe.appliance.toLowerCase() === tag.toLowerCase())
+//         );
+//         console.log('foundByApplianceTags', foundByApplianceTags)
+//         return foundBySearch && foundByIngredientTags && foundByApplianceTags;
+//     });
+// }
+
+
+
 export function filterRecipes(recipes, query, tags = []) {
-    //utilise filter pour parcourir et filtrer toutes les recettes
     return recipes.filter(recipe => {
-        // vérifie si le nom, la description, les ingrédients correspondent à la requête
+        // Vérifie si le nom, la description, ou les ingrédients correspondent à la requête
         const foundBySearch =
             recipe.name.toLowerCase().includes(query) ||
             recipe.description.toLowerCase().includes(query) ||
             recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(query));
 
-        const foundByTags = tags.length === 0 || tags.every(tag =>
-            recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase() === tag.toLowerCase())
-        );
+        // Vérifie si la recette contient tous les tags
+        const foundByTags = tags.length === 0 || tags.every(tag => {
+            // Vérifie si le tag est un ingrédient dans la recette
+            const isIngredientTag = recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase() === tag.toLowerCase());
+
+            // Vérifie si le tag est un appareil correspondant
+            const isApplianceTag = recipe.appliance.toLowerCase() === tag.toLowerCase();
+
+
+            // Vérifie si le tag est un appareil correspondant
+            const isUstensilTag = recipe.ustensils.some(ustensil => ustensil.toLowerCase() === tag.toLowerCase());
+
+            return isIngredientTag || isApplianceTag || isUstensilTag;
+        });
 
         return foundBySearch && foundByTags;
     });
---
+++ scripts/filters/ingredientsFilter.js
@@ -0,0 +1,131 @@
+export function ingredientsFilter(recipes, onTagSelect) {
+
+    // recupère les ingredients
+    const ingredientsSet = new Set();
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(ingredient => {
+            ingredientsSet.add(ingredient.ingredient);
+        });
+    });
+
+    const ingredientListContainer = document.getElementById('ingredientList');
+
+    /*****************************************************/
+
+
+    const activeTags = new Set();
+
+
+    /************************ ajoute la liste d'ingredients dans le dropdown **************************/
+    ingredientsSet.forEach(ingredient => {
+        const a = document.createElement('a');
+        a.href = "#";
+        a.className = "dropdown-list block";
+        a.textContent = ingredient;
+        a.addEventListener('click', (e) => {
+            e.preventDefault();
+            if (!a.classList.contains('active-tag')) {
+                a.classList.add('active-tag');
+
+                //ajout du bouton qui supprime la classe active
+                const removeActiveBtn = document.createElement('button');
+                removeActiveBtn.className = 'remove-active-btn';
+                removeActiveBtn.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
+                        <circle cx="8.5" cy="8.5" r="8.5" fill="black"/>
+                        <path d="M11 11L8.5 8.5M8.5 8.5L6 6M8.5 8.5L11 6M8.5 8.5L6 11" stroke="#FFD15B" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>`;
+
+                removeActiveBtn.addEventListener('click', (e) => {
+                    e.stopPropagation();
+                    a.classList.remove('active-tag');
+                    removeTag(ingredient);
+                    activeTags.delete(ingredient);
+                    removeActiveBtn.remove();
+                    onTagSelect(Array.from(activeTags));
+                });
+
+                a.appendChild(removeActiveBtn);
+                createTag(ingredient);
+                activeTags.add(ingredient);
+            } else {
+                a.classList.remove('active-tag');
+                removeTag(ingredient);
+                activeTags.delete(ingredient);
+                const removeButton = a.querySelector('.remove-active-btn');
+                if (removeButton) removeButton.remove();
+            }
+            onTagSelect(Array.from(activeTags));
+        });
+        ingredientListContainer.appendChild(a);
+    });
+
+
+
+    // compare la valeur de l'input pour afficher ou cacher les ingredient dans la liste
+    const searchInputIngredients = document.getElementById('searchInputIngredients');
+    searchInputIngredients.addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase();
+        const items = document.querySelectorAll('#ingredientList a');
+        items.forEach(item => {
+            const text = item.textContent.toLowerCase();
+            if (text.includes(searchTerm)) {
+                item.classList.remove('hidden');
+            } else {
+                item.classList.add('hidden');
+            }
+        });
+    });
+
+
+    function createTag(ingredientName) {
+        const tagContainer = document.querySelector('.tags-container');
+        const tag = document.createElement('div');
+        tag.className = 'tag';
+        tag.textContent = ingredientName;
+
+        // Ajoutez un attribut pour lier le tag au lien <a>
+        tag.setAttribute('data-ingredient', ingredientName);
+
+        const closeButton = document.createElement('button');
+        closeButton.className = 'ml-2 text-gray-500 hover:text-gray-700';
+        closeButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
+            <path d="M12 11.5L7 6.5M7 6.5L2 1.5M7 6.5L12 1.5M7 6.5L2 11.5" stroke="#1B1B1B" stroke-width="2.16667" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        `;
+
+        closeButton.addEventListener('click', () => {
+            removeTag(ingredientName);
+            activeTags.delete(ingredientName);
+            tag.remove();
+            onTagSelect(Array.from(activeTags));
+        });
+
+        tag.appendChild(closeButton);
+        tagContainer.appendChild(tag);
+    }
+
+
+    function removeTag(ingredientName) {
+        // supprime le tag du container de tags
+        const tags = document.querySelectorAll('.tags-container .tag');
+        tags.forEach(tag => {
+            if (tag.getAttribute('data-ingredient') === ingredientName) {
+                tag.remove();
+            }
+        });
+
+        // supprime la classe active-tag de l'élément <a> correspondant
+        const dropdownItems = document.querySelectorAll('#ingredientList a');
+        dropdownItems.forEach(item => {
+            if (item.textContent.trim() === ingredientName) {  // trim() enleve les espaces dans la valeur du de l'ingredient
+                item.classList.remove('active-tag');
+                const removeButton = item.querySelector('.remove-active-btn');
+                if (removeButton) removeButton.remove();
+            }
+        });
+    }
+
+}
+
