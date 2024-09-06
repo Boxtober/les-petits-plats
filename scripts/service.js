394dc632df6bfb04de36f99d5bcb91c471bf0215
sabrina <sabrina@verymountain.fr>
2024-09-06 12:28:13 +0200

    :wastebasket: Clean up

--- scripts/service.js
+++ scripts/service.js
@@ -1,6 +1,6 @@
 export async function getRecipes() {
     try {
-        const response = await fetch('../data/recipes.json');
+        const response = await fetch('data/recipes.json');
         if (!response.ok) {
             throw new Error('Erreur');
         }
